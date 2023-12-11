01169b9cf26b6baa1952f6e1c478c9ed06d4d69c
Simon <57576661+simon-stentoft@users.noreply.github.com>
2023-12-11 15:14:07 +0100

    CSVWriter class

---
+++ src/PaymentWriter.ts
@@ -0,0 +1,18 @@
+import { CSVWriter } from "./index"
+
+interface Payment {
+	id: number
+	amount: number
+	to: string
+	notes: string
+}
+
+const paymentWriter = new CSVWriter<Payment>(['id', 'amount', 'to', 'notes'])
+
+paymentWriter.addRows([
+	{ id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
+	{ id: 2, amount: 75, to: 'mario', notes: 'for web dev work' },
+    { id: 2, amount: 75, to: 'mario', notes: 'for web dev work' },
+])
+
+paymentWriter.save('./data/payments.csv')
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
+import { appendFileSync } from "fs"
 
 export class CSVWriter<T> {
+	constructor(private columns: (keyof T)[]) {
+		this.csv = this.columns.join(',') + '\n'
+	}
+
+	private csv: string
+
+	save(filename: string): void {
+		appendFileSync(filename, this.csv)
+		this.csv = '\n'
+
+		console.log('file saved to', filename)
+	}
+
+	addRows(values: T[]): void {
+		let rows = values.map((v) => this.formatRow(v))
+
+		this.csv += rows.join('\n')
+
+		console.log(this.csv)
+	}
+
+	private formatRow(value: T): string {
+		return this.columns.map((col) => value[col]).join(',')
+	}
+}
+
+class CSVReader {
     
 }
